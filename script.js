501105bbdd8a4c5067ab151f98cb5b0a88b626fd
Joshe1980 <126641808+Joshe1980@users.noreply.github.com>
2023-03-15 22:06:58 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+let hideText_btn = document.getElementById("hideText_btn");
+
+let hideText = document.getElementById("hideText");
+
+
+hideText_btn.addEventListener("click", toggleText);
+
+function toggleText() {
+    hideText.classList.toggle("show");
+}
+
+let hideText_btn2 = document.getElementById("hideText_btn2");
+
+let hideText2 = document.getElementById("hideText2");
+
+
+hideText_btn2.addEventListener("click", toggleText);
+
+function toggleText() {
+    hideText2.classList.toggle("show");
+}
