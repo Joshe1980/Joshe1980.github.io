4e3be30cd490d39a7ead3441c9129c8f5c942f9e
Joshe1980 <126641808+Joshe1980@users.noreply.github.com>
2023-03-19 17:29:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let hideText_btn = document.getElementById("hideText_btn");
+
+let hideText = document.getElementById("hideText");
+
+hideText_btn.addEventListener("click", toggleText);
+
+function toggleText() {
+    hideText.classList.toggle("show");
+}
+
+let hideText_btn2 = document.getElementById("hideText_btn2");
+
+let hideText2 = document.getElementById("hideText2");
+
+hideText_btn2.addEventListener("click", toggleText2);
+
+function toggleText2() {
+    hideText2.classList.toggle("show");
+}
+
+let hideText_btn3 = document.getElementById("hideText_btn3");
+
+let hideText3 = document.getElementById("hideText3");
+
+hideText_btn3.addEventListener("click", toggleText3);
+
+function toggleText3() {
+    hideText3.classList.toggle("show");
+}
